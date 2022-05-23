4a695a466c3760e3385d0a0112f04d0dd0c0f666
Giovanni Siracusa <giovanni.siracusa@mail.polimi.it>
2022-05-23 17:31:10 +0200

    Update env.py

--- env.py
+++ env.py
@@ -2,23 +2,45 @@ import numpy as np
 from copy import copy
 
 
-def simulate_episode(init_prob_matrix, n_steps_max):
+def simulate_episode(init_prob_matrix, n_steps_max, initial_active_node, l=0.8):
     prob_matrix = init_prob_matrix.copy()
-    n_nodes = prob_matrix.shape[0]
-    initial_active_nodes = np.random.binomial(1, 0.1, size=(n_nodes))
-    history = np.array([initial_active_nodes])
-    active_nodes = initial_active_nodes
+    np.fill_diagonal(prob_matrix, 0)
+    history = np.array([initial_active_node])
+    active_nodes = np.array([initial_active_node])  # array di active nodes esempio: se active_nodes sono 2 e 3 allora sarà [[0,1,0,0,0], [0,0,1,0,0]]
     newly_active_nodes = active_nodes
     t = 0
     while (t < n_steps_max and np.sum(newly_active_nodes) > 0):
-        p = (prob_matrix.T * active_nodes).T
+        active_node = active_nodes[0].copy()  # Prendi il primo active node in coda
+        active_nodes = active_nodes[1:]
+        idx_active = np.argwhere(active_node).reshape(-1)
+
+        p = (prob_matrix.T * active_node).T
+        temp = np.argsort(p[idx_active]).reshape(-1)
+
+        for idx in range(len(p[idx_active].reshape(-1))):
+            if temp[-1] != idx and temp[-2] != idx:
+                p[idx_active, idx] = 0
+            if temp[-2] == idx:
+                p[idx_active, idx] = p[idx_active, idx] * l
+
         activated_edges = p > np.random.rand(p.shape[0], p.shape[1])  # AGGIUNGERE I RESERVATION PRICE E LAMBDA
-        prob_matrix = prob_matrix * ((p != 0) == activated_edges)
-        newly_active_nodes = (np.sum(activated_edges, axis=0) > 0) * (1 - active_nodes)
-        active_nodes = np.array(active_nodes + newly_active_nodes)
-        history = np.concatenate((history, [newly_active_nodes]), axis=0)
+        # prob_matrix = prob_matrix * ((p != 0) == activated_edges)
+
+        prob_matrix[:, idx_active] = 0
+        newly_active_nodes = (np.sum(activated_edges, axis=0) > 0) * (1 - active_node)
+
+        for idx, val in enumerate(newly_active_nodes):
+            if val == 1:
+                a = np.zeros(5)
+                a[idx] = 1
+                active_nodes = np.concatenate((active_nodes, [a]), axis=0)
+
+        active_nodes = np.unique(active_nodes, axis=0)
+        # print(active_nodes, end='\n\n')
+        history = np.concatenate((history, [active_node]), axis=0)
         t += 1
-    return history
+    # print(prob_matrix)
+    return history[1:]
 
 
 def estimate_probabilities(dataset, node_index, n_nodes):
@@ -43,20 +65,27 @@ def estimate_probabilities(dataset, node_index, n_nodes):
 
 n_nodes = 5
 n_episodes = 1000
-prob_matrix = np.random.uniform(0.0, 0.25, (n_nodes, n_nodes))
+prob_matrix = np.random.uniform(0.9, 0.99, (n_nodes, n_nodes))
 
-nodes = np.array(range(6))
+nodes = np.array(range(n_nodes + 1))
 alphas = np.random.dirichlet(np.ones(6), size=1).reshape(-1)
-initial_node = np.random.choice(nodes, 1, p=alphas)
 
-node_index = np.array([4])
+
 dataset = []
+node_index = np.array([4])
 
-for e in range(0, n_episodes):
-    dataset.append(simulate_episode(init_prob_matrix=prob_matrix, n_steps_max=10))
+for e in range(0, 1000):
+    initial_node = np.random.choice(nodes, 1, p=alphas)
+    initial_active_node = np.zeros(n_nodes + 1)
+    initial_active_node[initial_node] = 1
+    initial_active_node = initial_active_node[1:]
+    dataset.append(
+        simulate_episode(init_prob_matrix=prob_matrix, n_steps_max=10, initial_active_node=initial_active_node))
+    #print(dataset)
+
 for n in node_index:
-    estimated_prob = estimate_probabilities(dataset=dataset, node_index=n, n_nodes=n_nodes)
+    estimated_prob = estimate_probabilities(dataset=dataset, node_index=1, n_nodes=n_nodes)
     print(f"{n}:{estimated_prob}")
 
 print("True P Matrix: ")
-print(prob_matrix)
+print(prob_matrix.T)
