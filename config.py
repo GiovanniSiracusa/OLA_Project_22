7c1b9e26a149c199f2c5e03dc941b73a021876a6
Giovanni Siracusa <giovanni.siracusa@mail.polimi.it>
2022-06-05 12:00:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,22 @@
 import numpy as np
 
-conversion_rates1 = np.random.rand(5, 5)
+# DEFINIRE CONVERSION RATES PER OGNI CLASSE DI UTENTI
+conversion_rates1 = np.array([0.7, 0.5, 0.3, 0.15, 0.1],
+                             [0.5, 0.3, 0.2, 0.1, 0.01],
+                             [0.9, 0.7, 0.4, 0.2, 0.1],
+                             [0.3, 0.25, 0.2, 0.1, 0.05],
+                             [0.4, 0.35, 0.3, 0.25, 0.2])
 conversion_rates2 = np.random.rand(5, 5)
 conversion_rates3 = np.random.rand(5, 5)
 
+# DEFINIRE I PREZZI DI OGNI PRODOTTO
+prices1 = np.array([10, 12, 15, 18, 20])
+prices2 = np.array([10, 12, 15, 18, 20])
+prices3 = np.array([10, 12, 15, 18, 20])
+prices4 = np.array([10, 12, 15, 18, 20])
+prices5 = np.array([10, 12, 15, 18, 20])
+
+# DEFINIRE ALPHAS PER OGNI CLASSE DI UTENTI
 alphas1 = np.array([0.1, 0.18, 0.18, 0.18, 0.18])
 alphas2 = np.array([0.1, 0.18, 0.18, 0.18, 0.18])
 alphas3 = np.array([0.1, 0.18, 0.18, 0.18, 0.18])
@@ -19,6 +32,11 @@ max_sold_items1 = 5
 max_sold_items2 = 5
 max_sold_items3 = 5
 
-graph_probs1 = np.random.rand(5, 5)
+# DEFINIRE PROBABILITA DI PASSARE DA UN PRODOTTO A UN ALTRO PER OGNI CLASSE DI UTENTI
+graph_probs1 = np.array([0.6, 0.5, 0.3, 0.15, 0.1],
+                        [0.5, 0.3, 0.2, 0.1, 0.01],
+                        [0.9, 0.7, 0.4, 0.2, 0.1],
+                        [0.3, 0.25, 0.2, 0.1, 0.05],
+                        [0.4, 0.35, 0.3, 0.25, 0.2])
 graph_probs2 = np.random.rand(5, 5)
 graph_probs3 = np.random.rand(5, 5)
